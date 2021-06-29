f3fb5bc72b80435a6a207c9e163477d17c8f8b5c
yuanchenghao <188897876@qq.com>
2021-06-29 18:22:16 +0800

    。。。

--- app/src/main/java/ch/study/ch/MainActivity.kt
+++ app/src/main/java/ch/study/ch/MainActivity.kt
@@ -2,6 +2,7 @@ package ch.study.ch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//Java泛型机制7连问
 //https://juejin.cn/post/6978833860284907527
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
